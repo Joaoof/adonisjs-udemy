ff003d9ed5f9a2624c4f776d9b01d10ed765deb6
joao <joaodeus400@gmail.com>
2023-07-20 08:49:54 -0300

    hash password user

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,5 +1,5 @@
-import { Hash } from '@adonisjs/core/build/standalone'
-import { BaseModel, beforeSave /* ... */, column } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 
 export default class User extends BaseModel {
@@ -7,16 +7,16 @@ export default class User extends BaseModel {
   public id: number
 
   @column()
-  public username: string
-
-  @column()
   public email: string
 
   @column()
-  public avatar: string
+  public username: string
 
-  @column({ serializeAs: null })
+  @column({ serializeAs: null }) // modelos de saida da senha não são perceptiveis, por conta do serialize.
   public password: string
+
+  @column()
+  public avatar: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
@@ -27,7 +27,7 @@ export default class User extends BaseModel {
   @beforeSave()
   public static async hashPassword(user: User) {
     if (user.$dirty.password) {
-      user.password = await Hash.apply(user.password)
+      user.password = await Hash.make(user.password)
     }
   }
 }
--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -4,18 +4,20 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', () => {
   test.only('it should create an user!', async (assert) => {
-    const user = {
+    const userPayload = {
       email: 'test@test.com',
       username: 'test',
       password: 'teste',
       avatar: 'https://images.com/image/1',
     }
-    const { body } = await supertest(BASE_URL).post('/users').send(user).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    console.log(body)
+
     assert.exists(body.user, 'User undefined')
     assert.exists(body.user.id, 'Id undefined')
-    assert.equal(body.user.email, user.email)
-    assert.equal(body.user.username, user.username)
-    assert.equal(body.user.avatar, user.avatar)
-    assert.equal(body.user.password, user.password)
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.avatar, userPayload.avatar)
+    assert.notExists(body.user.password, 'Password defined')
   })
 })
