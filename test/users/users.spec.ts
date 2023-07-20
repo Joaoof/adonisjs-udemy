ab5905ee3ca9fce7486efb17689234abdf54ec0a
joao <joaodeus400@gmail.com>
2023-07-20 09:59:01 -0300

    Transações nos testes e checagem do email em uso no controller

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,9 +1,10 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
-test.group('User', () => {
+test.group('User', (group) => {
   test('it should create an user!', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
@@ -22,7 +23,7 @@ test.group('User', () => {
     assert.notExists(body.user.password, 'Password defined')
   })
 
-  test('it should return 409 when email is already in use', async (assert) => {
+  test.only('it should return 409 when email is already in use', async (assert) => {
     const { email } = await UserFactory.create() // API SENDO FEITA PELA FACTORIES
     const { body } = await supertest(BASE_URL)
       .post('/users')
@@ -32,5 +33,20 @@ test.group('User', () => {
         password: 'teste',
       })
       .expect(409)
+
+    assert.exists(body.message)
+    assert.exists(body.code)
+    assert.exists(body.status)
+    assert.include(body.message, 'email')
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
   })
 })
