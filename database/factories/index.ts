764791dba07a43ff07f09d91d0b14a789d4dd707
joao <joaodeus400@gmail.com>
2023-07-26 16:25:02 -0300

    comentarios pra ajudar a entender a api

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -9,3 +9,5 @@ export const UserFactory = Factory.define(User, ({ faker }) => {
     avatar: faker.internet.url(),
   }
 }).build()
+
+//  A Factory é uma classe que é usada para criar instâncias de modelos de banco de dados com dados falsos para testes. No código fornecido, a Factory define um usuário com um nome de usuário, e-mail, senha e avatar falsos usando a biblioteca faker. A função build() é usada para criar a instância do modelo com os dados falsos. A Factory é usada em conjunto com o teste unitário para criar um usuário de teste e verificar se a API funciona corretamente.
--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,19 +1,21 @@
-import Database from '@ioc:Adonis/Lucid/Database'
-import { UserFactory } from 'Database/factories'
-import test from 'japa'
-import supertest from 'supertest'
+import Database from '@ioc:Adonis/Lucid/Database' // Importa o módulo Database do Adonis.js para interagir com o banco de dados
+import { UserFactory } from 'Database/factories' // importa a classe UserFactory do módulo Database/factories para criar usuários de teste
+import test from 'japa' // importa a biblioteca Japa para escrever testes unitários
+import supertest from 'supertest' //  importa a biblioteca supertest para fazer requisições HTTP durante os testes.
 
-const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}` // Define a URL base da API com base nas variáveis de ambiente HOST e PORT.
 test.group('User', (group) => {
+  // Define um grupo de testes chamado "User".
   test('it should create an user!', async (assert) => {
+    // Define um teste dentro do grupo "User" que verifica se um usuário é criado corretamente.
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
       password: 'teste',
       avatar: 'https://images.com/image/1',
-    }
+    } // Define um objeto com os dados do usuário que será criado.
     const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
-    console.log(body)
+    console.log(body) // Faz uma requisição POST para a rota '/users' da API com os dados do usuário e espera que a resposta tenha o código 201 (Created). O corpo da resposta é armazenado na variável body e é exibido no console.
 
     assert.exists(body.user, 'User undefined')
     assert.exists(body.user.id, 'Id undefined')
@@ -21,17 +23,18 @@ test.group('User', (group) => {
     assert.equal(body.user.username, userPayload.username)
     assert.equal(body.user.avatar, userPayload.avatar)
     assert.notExists(body.user.password, 'Password defined')
-  })
+  }) // Realiza várias asserções para verificar se o objeto do usuário retornado na resposta possui as propriedades esperadas e se a senha não está presente.
 
   test.only('it should return 409 when email is already in use', async (assert) => {
-    const { email } = await UserFactory.create() // API SENDO FEITA PELA FACTORIES
+    // Define um teste dentro do grupo "User" que verifica se a API retorna o código 409 (Conflict) quando o e-mail já está em uso.
+    const { email } = await UserFactory.create() // API SENDO FEITA PELA FACTORIES. Cria um usuário de teste utilizando a classe UserFactory e armazena o e-mail gerado na variável email
     const { body } = await supertest(BASE_URL)
       .post('/users')
       .send({
         email,
         username: 'teste',
         password: 'teste',
-      })
+      }) // Faz uma requisição POST para a rota '/users' da API com o e-mail já em uso e espera que a resposta tenha o código 409 (Conflict). O corpo da resposta é armazenado na variável body e é exibido no console.
       .expect(409)
     console.log({ body })
     assert.exists(body.message)
@@ -40,7 +43,7 @@ test.group('User', (group) => {
     assert.include(body.message, 'email')
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 409)
-  })
+  }) // Realiza várias asserções para verificar se o corpo da resposta possui as propriedades esperadas e se a mensagem de erro contém a palavra "email".
 
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
@@ -48,5 +51,5 @@ test.group('User', (group) => {
 
   group.afterEach(async () => {
     await Database.rollbackGlobalTransaction()
-  })
+  }) // Define hooks que são executados antes e depois de cada teste no grupo. Esses hooks iniciam e revertem uma transação global do banco de dados, garantindo que cada teste seja executado em um ambiente isolado.
 })
