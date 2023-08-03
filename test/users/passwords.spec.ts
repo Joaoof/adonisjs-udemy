108863915f53c1f2da5c2d69d37c16a0145e9059
joao <joaodeus400@gmail.com>
2023-08-03 17:45:07 -0300

    erro no json

--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -7,7 +7,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Password', (group) => {
-  test.only('it should send email with forgot password instructions', async (assert) => {
+  test('it should send email with forgot password instructions', async (assert) => {
     const user = await UserFactory.create()
 
     Mail.trap((message) => {
@@ -31,6 +31,19 @@ test.group('Password', (group) => {
       .expect(204)
 
     Mail.restore()
+  })
+
+  test('it should create a reset password token', async (assert) => {
+    const user = await UserFactory.create()
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
+      })
+      .expect(204)
+
+    user.related('tokens')
   })
 
   group.beforeEach(async () => {
