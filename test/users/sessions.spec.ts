510b322007c39074f82060d8809b277a38eb0a57
joao <joaodeus400@gmail.com>
2023-08-08 08:12:31 -0300

    testando geração de token api

--- test/users/sessions.spec.ts
+++ test/users/sessions.spec.ts
@@ -6,7 +6,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Session', (group) => {
-  test.only('it should autenticate an user', async (assert) => {
+  test('it should autenticate an user', async (assert) => {
     const plainPassword = 'test' // senha em texto puro
     const { id, email } = await UserFactory.merge({
       password: plainPassword,
@@ -20,11 +20,24 @@ test.group('Session', (group) => {
     assert.equal(body.user.id, id)
   })
 
+  test.only('it should return an api token when session is created', async (assert) => {
+    const plainPassword = 'test' // senha em texto puro
+    const { id, email } = await UserFactory.merge({
+      password: plainPassword,
+    }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    assert.isDefined(body.token, 'Token undefined ')
+    assert.equal(body.user.id, id)
+  })
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
 
-  group.beforeEach(async () => {
-    await Database.beginGlobalTransaction()
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
   })
 })
